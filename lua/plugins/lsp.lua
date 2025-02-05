be27c1dda3c61816fd88a424463a9deb5179d993
tanaka hideyuki <thdyk.4.11@gmail.com>
2025-02-05 20:13:29 +0900

    [add] cmp plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,113 +8,26 @@ return {
       "BufWritePre",
     },
     dependencies = {
-      { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
-      { "folke/neodev.nvim",  opts = {} },
-      "mason.nvim",
+      -- mason
+      "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
       -- cmp
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/cmp-vsnip",
-      "hrsh7th/vim-vsnip",
+      'saghen/blink.cmp',
     },
-    config = function(_, _)
-      require("mason-lspconfig").setup_handlers({ function(server)
-        local cmp = require('cmp')
-        cmp.setup({
-          snippet = {
-            -- REQUIRED - you must specify a snippet engine
-            expand = function(args)
-              vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            end,
-          },
-          window = {
-            -- completion = cmp.config.window.bordered(),
-            -- documentation = cmp.config.window.bordered(),
-          },
-          mapping = cmp.mapping.preset.insert({
-            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<C-e>'] = cmp.mapping.abort(),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          }),
-          sources = cmp.config.sources({
-            { name = 'nvim_lsp' },
-            { name = 'vsnip' }, -- For vsnip users.
-          }, {
-            { name = 'buffer' },
-          })
-        })
-
-        -- Set configuration for specific filetype.
-        cmp.setup.filetype('gitcommit', {
-          sources = cmp.config.sources({
-            { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
-          }, {
-            { name = 'buffer' },
-          })
-        })
-
-        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline({ '/', '?' }, {
-          mapping = cmp.mapping.preset.cmdline(),
-          sources = {
-            { name = 'buffer' }
-          }
-        })
-
-        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline(':', {
-          mapping = cmp.mapping.preset.cmdline(),
-          sources = cmp.config.sources({
-            { name = 'path' }
-          }, {
-            { name = 'cmdline' }
-          })
-        })
-        local opts = {
-          capabilities = require("cmp_nvim_lsp").default_capabilities()
-        }
-        require("lspconfig")[server].setup(opts)
-      end })
-    end
-  },
-
-  -- cmdline tools and lsp servers
-  {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    build = ":MasonUpdate",
-    opts = {},
     config = function(_, opts)
-      require("mason").setup(opts)
-      local mr = require("mason-registry")
-      mr:on("package:install:success", function()
-        vim.defer_fn(function()
-          -- trigger FileType event to possibly load this newly installed LSP server
-          require("lazy.core.handler.event").trigger({
-            event = "FileType",
-            buf = vim.api.nvim_get_current_buf(),
+      local mason = require("mason")
+      local mason_lspconfig = require("mason-lspconfig")
+      local lspconfig = require('lspconfig')
+
+      mason.setup()
+      mason_lspconfig.setup()
+      mason_lspconfig.setup_handlers({
+        function(server)
+          lspconfig[server].setup({
+            capabilities = require('blink.cmp').get_lsp_capabilities({})
           })
-        end, 100)
-      end)
-      local function ensure_installed()
-        for _, tool in ipairs(opts.ensure_installed or {}) do
-          local p = mr.get_package(tool)
-          if not p:is_installed() then
-            p:install()
-          end
         end
-      end
-      if mr.refresh then
-        mr.refresh(ensure_installed)
-      else
-        ensure_installed()
-      end
+      })
     end,
   },
 }
--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,41 +1,26 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = {
-      "BufReadPost",
-      "BufNewFile",
-      "BufWritePre",
-    },
-    dependencies = {
-      { "nvim-treesitter/nvim-treesitter-textobjects" },
-    },
-    opts_extend = { "ensure_installed", },
-    opts = {
-      highlight = { enable = true, },
-      indent = { enable = true, },
-      ensure_installed = {
-        -- markview.nvim
-        "html",
-        "markdown",
-        "markdown_inline",
-      }
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = {
+    "BufReadPost",
+    "BufNewFile",
+    "BufWritePre",
   },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    event = {
-      "BufReadPost",
-      "BufNewFile",
-      "BufWritePre",
-    },
-    opts = {
-      max_lines = 1,
-      multiline_threshold = 1,
-      mode = "topline",
-    },
-  }
+  dependencies = {
+    { "nvim-treesitter/nvim-treesitter-textobjects" },
+  },
+  opts_extend = { "ensure_installed", },
+  opts = {
+    highlight = { enable = true, },
+    indent = { enable = true, },
+    ensure_installed = {
+      -- markview.nvim
+      "html",
+      "markdown",
+      "markdown_inline",
+    }
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end
 }
